8a13b289a7c87ad29a2b6061a641e5a68d307210
Username4Fabian <03anion.proxys@icloud.com>
2023-05-11 23:12:43 +0200

    Working with First and Lastname

---
+++ Main.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import openpyxl
+
+# Email presets
+email_formats = ["{f}.{l}@example.com", "{f}{l}@example.com", "{f}_{l}@example.com"]
+
+# Function to generate email addresses
+def generate_emails(first_name, last_name):
+    emails = []
+    for email_format in email_formats:
+        email = email_format.format(f=first_name, l=last_name)
+        emails.append(email)
+    return emails
+
+# Function to get names from user
+def get_names():
+    names = []
+    line_counter = 1  # Initialize line counter
+
+    while True:
+        print("Enter a name (or 'done' to finish):")
+        full_name = input("Full name: ").strip()
+        if full_name.lower() == 'done':
+            break
+        first_name, last_name = full_name.split(maxsplit=1)
+        names.append((first_name, last_name))
+        
+        # Load the workbook
+        workbook = openpyxl.load_workbook('emails.xlsx')
+
+        # Select the first sheet (you can modify this if you have multiple sheets)
+        sheet = workbook.active
+
+        # Find the last line counted in column A
+        while sheet.cell(row=line_counter, column=1).value is not None:
+            line_counter += 1
+
+        # Generate emails for the current name
+        all_emails = generate_emails(first_name, last_name)
+
+        # Write the emails to the next line
+        for email in all_emails:
+            sheet.cell(row=line_counter, column=1).value = email
+
+            # Increment the line counter
+            line_counter += 1
+
+        # Save the workbook
+        workbook.save('emails.xlsx')
+
+    return names
+
+# Get names from user
+names = get_names()
